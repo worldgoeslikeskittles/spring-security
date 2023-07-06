be646f365c641d36dc33130274ba1a00d6fa1f4c
ppkislov@domclick.ru <ppkislov@domclick.ru>
2023-07-06 06:42:11 +0300

    login:password roles fixed

--- spring-security-old/src/main/kotlin/com/example/springsecurityold/domain/enumerated/Role.kt
+++ spring-security-old/src/main/kotlin/com/example/springsecurityold/domain/enumerated/Role.kt
@@ -2,13 +2,13 @@ package com.example.springsecurityold.domain.enumerated
 
 import org.springframework.security.core.GrantedAuthority
 
-enum class Role(): GrantedAuthority {
-    CLIENT,
-    DOCTOR,
-    MANAGER,
-    ADMIN;
+enum class Role(private val authority: String): GrantedAuthority {
+    CLIENT("ROLE_CLIENT"),
+    DOCTOR("ROLE_DOCTOR"),
+    MANAGER("ROLE_MANAGER"),
+    ADMIN("ROLE_ADMIN");
 
     override fun getAuthority(): String {
-        return this.name
+        return this.authority
     }
 }
